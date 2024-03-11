be453d32ffd5eb8e6a796baee844248ff6b9d0d5
azharrizal06 <azharrizal06@gmail.com>
2024-03-11 14:48:28 +0700

    hapus nav

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -2,8 +2,6 @@ import 'package:get/get.dart';
 
 import '../modules/home/bindings/home_binding.dart';
 import '../modules/home/views/home_view.dart';
-import '../modules/nav/bindings/nav_binding.dart';
-import '../modules/nav/views/nav_view.dart';
 
 part 'app_routes.dart';
 
@@ -17,11 +15,6 @@ class AppPages {
       name: _Paths.HOME,
       page: () => HomeView(),
       binding: HomeBinding(),
-    ),
-    GetPage(
-      name: _Paths.NAV,
-      page: () => NavView(),
-      binding: NavBinding(),
     ),
   ];
 }
